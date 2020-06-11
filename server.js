078f89c5322e23593f8e8e52bdf50428ba3a232d
jasonstevens13 <jasonstevens13@yahoo.com>
2020-06-11 00:45:35 -0400

    fixed img class in giphed article cards to adhere to bootstrap img bottom

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 // *** Dependencies
 // =============================================================
 const express = require("express");
-require('dotenv').config();
+
 
 // Sets up the Express App
 // =============================================================
